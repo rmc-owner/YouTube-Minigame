cb4a1103cbe91b814c7868f35a8135afc2e78083
BetaNyan <b3tanyan@gmail.com>
2016-04-22 14:39:47 -0400

    Episode 3 - Signs

--- src/main/java/pw/betanyan/minigame/listeners/PlayerInteract.java
+++ src/main/java/pw/betanyan/minigame/listeners/PlayerInteract.java
@@ -1,10 +1,12 @@
 package pw.betanyan.minigame.listeners;
 
 import net.md_5.bungee.api.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import pw.betanyan.minigame.Minigame;
 import pw.betanyan.minigame.game.Arena;
@@ -20,17 +22,25 @@ public class PlayerInteract implements Listener {
     @EventHandler
     public void onInt(PlayerInteractEvent e) {
 
-        FileConfiguration config = plugin.getConfig();
+        if (e.getAction() == Action.RIGHT_CLICK_BLOCK) {
 
-        for (String arena : config.getConfigurationSection("arenas").getKeys(false)) {
+            if (e.getClickedBlock().getType() == Material.SIGN_POST || e.getClickedBlock().getType() == Material.SIGN) {
 
-            if (config.getStringList("arenas." + arena + ".signs").contains(plugin.serializeLocation(e.getClickedBlock().getLocation()))) {
+                FileConfiguration config = plugin.getConfig();
 
-                Arena arenaObj = plugin.getArenaManager().getArenaByName(arena);
+                for (String arena : config.getConfigurationSection("arenas").getKeys(false)) {
 
-                arenaObj.getIngame().add(e.getPlayer().getName());
-                arenaObj.broadcast("&a" +  e.getPlayer().getName() + " has joined the game! " + arenaObj.getIngame().size() + "/" + arenaObj.getMaxPlayers());
-                arenaObj.getSign().setLine(2, String.valueOf(ChatColor.YELLOW) + arenaObj.getIngame().size() + "/" + arenaObj.getMaxPlayers());
+                    if (config.getStringList("arenas." + arena + ".signs").contains(plugin.serializeLocation(e.getClickedBlock().getLocation()))) {
+
+                        Arena arenaObj = plugin.getArenaManager().getArenaByName(arena);
+
+                        arenaObj.getIngame().add(e.getPlayer().getName());
+                        arenaObj.broadcast("&a" + e.getPlayer().getName() + " has joined the game! " + arenaObj.getIngame().size() + "/" + arenaObj.getMaxPlayers());
+                        arenaObj.getSign().setLine(2, String.valueOf(ChatColor.YELLOW) + arenaObj.getIngame().size() + "/" + arenaObj.getMaxPlayers());
+
+                    }
+
+                }
 
             }
 
